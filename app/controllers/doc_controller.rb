405e64099240960d05977933516ce4fcfe0956b4
conor <conor@016420e5-f504-0410-9c4b-e4e4ac7be119>
2005-11-28 05:11:35 +0000

    fixed bug with source display
    git-svn-id: file:///Users/conorhunt/Code/Rails/svndavrepo/rannotate/branches/no_frames@32 016420e5-f504-0410-9c4b-e4e4ac7be119

--- app/controllers/doc_controller.rb
+++ app/controllers/doc_controller.rb
@@ -181,7 +181,7 @@ class DocController < ApplicationController
   # Render the source inline
   def render_source(id)
     @source_code = RaSourceCode.find(id).source_code
-    render :partial => 'source_code'
+    render :partial => 'partials/source_code'
   end
 
 end
